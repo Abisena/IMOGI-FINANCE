f8fd9338b24ef541e4d27b3f154f41caff43f2f3
AbisenaP <91299253+Abisena@users.noreply.github.com>
2025-12-30 16:49:18 +0700

    Add Expense Request Item doctype module

---
+++ imogi_finance/imogi_finance/doctype/expense_request_item/expense_request_item.py
@@ -0,0 +1,12 @@
+# Copyright (c) 2024, Imogi and contributors
+# For license information, please see license.txt
+
+from __future__ import annotations
+
+from frappe.model.document import Document
+
+
+class ExpenseRequestItem(Document):
+    """Child table for line items within an Expense Request."""
+
+    pass
